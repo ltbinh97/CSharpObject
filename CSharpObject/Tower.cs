4f91e84b0a0f40091ea1270ccc860eb0b4062ad8
lthanhbinh1997 <lthanhbinh1997@gmail.com>
2018-12-04 00:55:52 +0700

    Finish C# Object Course

--- CSharpObject/Tower.cs
+++ CSharpObject/Tower.cs
@@ -8,14 +8,20 @@ namespace CSharpObject
     {
         private const int _range = 1;
         private const int _power = 1;
+        private const double _accuracy = .75;
 
-        private static readonly System.Random _random = new System.Random();
+        private static readonly Random _random = new Random();
 
         private readonly MapLocation _location;
 
         public Tower(MapLocation location)
         {
             _location = location;
+        }
+
+        public bool IsSuccessfulShot()
+        {
+            return _random.NextDouble() < _accuracy;
         }
 
         public void FireOnInvaders(Invader[] invaders)
@@ -25,7 +31,20 @@ namespace CSharpObject
                 // Do stuff with invader
                 if (invader.IsActive && _location.InRangeOf(invader.Location, _range))
                 {
-                    invader.DecreaseHealth(_power);
+                    if (IsSuccessfulShot())
+                    {
+                        invader.DecreaseHealth(_power);
+                        Console.WriteLine("Shot at and hit an invader!");
+
+                        if (invader.IsNeutralized)
+                        {
+                            Console.WriteLine("Neutralized an invader.");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Shot at and missed an invader.");
+                    }
                     continue;
                 }
             }
